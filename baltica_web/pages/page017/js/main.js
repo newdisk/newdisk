859683fa264fbfaf3fe98b4ead0a200758196948
NovyDisk <elearning.nd@gmail.com>
2017-02-14 18:09:43 +0300

    Baltica update

--- baltica_web/pages/page017/js/main.js
+++ baltica_web/pages/page017/js/main.js
@@ -1,19 +1,25 @@
 ;(function($, ctrl){
   'use strict';
+  var bable1 = $(parent.frames['myframe'].document).find('.del');
 
-  var pageBlock2Btn = $(parent.frames['myframe'].document).find('.page__bubble_down_lef');
+
+
+
+
+  var pageBlock2Btn = $(parent.frames['myframe'].document).find('.contactBtn');
   var pageContainer = $(parent.frames['myframe'].document).find('.container');
   var pageContainer1 = $(parent.frames['myframe'].document).find('.container_page1');
- // var pageContainer2 = $(parent.frames['myframe'].document).find('.container_page2');
+ //var pageContainer2 = $(parent.frames['myframe'].document).find('.container_page2');
   var bubble_user_name = $(parent.frames['myframe'].document).find('.user_name');
   bubble_user_name.html(ctrl.learner);
 
   pageBlock2Btn.on("click",handler);
 
   function handler(){
-    pageContainer.css('background', 'url("img/14_2.jpg") no-repeat');
-    pageContainer1.css('display', 'none');
-    pageContainer2.css('display', 'block');
+
+    $(pageContainer).css('background', 'url("img/14_2.jpg") no-repeat');
+    $(pageContainer1).css('display', 'none');
+    $(bable1).css('display', 'none');
   }
 
 
