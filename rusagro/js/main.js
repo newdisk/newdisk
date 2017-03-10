5b391e9389d3756f1c982b0caece87e5825e1cb7
NovyDisk <elearning.nd@gmail.com>
2017-03-10 16:44:58 +0300

    rusagro update

--- rusagro/js/main.js
+++ rusagro/js/main.js
@@ -181,107 +181,6 @@ angular
 });
 angular
 .module('app')
-.controller('AccordionController', function($scope, $element, $attrs) {
-
-  var self = this;
-  var panels = [];
-  // here we take the panel and add to our list of panels
-  // to preselect the first panel we call turnOn function on the first panel
-  self.addPanel = function(panel) {
-    panels.push(panel);
-    if ($attrs.firstOpen === 'true' && panel === panels[0]) {
-      return false;
-    }
-    return true;
-  };
-  // when a panel is selected we would want to open the content
-  // here we take the panel find it in our array and turn if on if not selected
-  // and off it.
-  self.selectPanel = function(panel,isCollapsed) {
-    for (var i in panels) {
-      if (panel === panels[i]) {
-        if (isCollapsed) {
-          panels[i].turnOn();
-        } else {
-          panels[i].turnOff();
-        }
-      } else {
-        panels[i].turnOff();
-      }
-    }
-  };
-})
-.component('accordion', {
-  transclude: true,
-  template:'<div class="accordion" ng-transclude></div>',
-  controller: 'AccordionController'
-})
-.controller('AccordionPanelCtrl', function($scope, $element, $attrs) {
-  
-  var self = this;
-  $scope.isCollapsed = true;
-  
-  // register the panel in init
-  self.$onInit = function () {
-    $scope.isCollapsed = self.parent.addPanel(self);
-  };
-  
-  // Turns on the panel 
-  self.turnOn = function () {
-    $scope.isCollapsed = false;
-  };
-  
-  // Turns off the panel 
-  self.turnOff = function () {
-    $scope.isCollapsed = true;
-  };
-  
-  $scope.toggle = function() {
-    // $scope.isCollapsed = !$scope.isCollapsed;
-    self.parent.selectPanel(self,$scope.isCollapsed);
-  };
-})
-.component('accordionPanel', {
-  require: {
-    'parent': '^accordion'
-  },
-  bindings: {
-    heading: '@'
-  },
-  transclude: true,
-  templateUrl: 'js/components/accordion/accordionPanelTmpl.html',
-  controller: 'AccordionPanelCtrl'
-})
-.controller('MyCtrl', function($scope, $element, $attrs) {
-  var element = $element[0];
-  
-  $scope.$watch($attrs.collapse, function (collapse) {
-    
-    var newHeight = collapse ? 0 : getElementAutoHeight();
-
-		element.style.height = newHeight + 'px';
-  })
-  
-  function getElementAutoHeight() {
-    var currentHeight = getElementCurrentHeight();
-    
-    element.style.height = 'auto';
-    var autoHeight = getElementCurrentHeight();
-    
-    element.style.height = currentHeight;
-    // Force the browser to recalc height after moving it back to normal
-    getElementCurrentHeight(); 
-    
-    return autoHeight;
-  }
-  
-  function getElementCurrentHeight() {
-    return element.offsetHeight
-  }
-})
-
-angular
-.module('app')
 .component('popupMenuComment', {
 	bindings: {
 		title: '@',
@@ -832,6 +731,107 @@ angular
 
   });
 })
+angular
+.module('app')
+.controller('AccordionController', function($scope, $element, $attrs) {
+
+  var self = this;
+  var panels = [];
+  // here we take the panel and add to our list of panels
+  // to preselect the first panel we call turnOn function on the first panel
+  self.addPanel = function(panel) {
+    panels.push(panel);
+    if ($attrs.firstOpen === 'true' && panel === panels[0]) {
+      return false;
+    }
+    return true;
+  };
+  // when a panel is selected we would want to open the content
+  // here we take the panel find it in our array and turn if on if not selected
+  // and off it.
+  self.selectPanel = function(panel,isCollapsed) {
+    for (var i in panels) {
+      if (panel === panels[i]) {
+        if (isCollapsed) {
+          panels[i].turnOn();
+        } else {
+          panels[i].turnOff();
+        }
+      } else {
+        panels[i].turnOff();
+      }
+    }
+  };
+})
+.component('accordion', {
+  transclude: true,
+  template:'<div class="accordion" ng-transclude></div>',
+  controller: 'AccordionController'
+})
+.controller('AccordionPanelCtrl', function($scope, $element, $attrs) {
+  
+  var self = this;
+  $scope.isCollapsed = true;
+  
+  // register the panel in init
+  self.$onInit = function () {
+    $scope.isCollapsed = self.parent.addPanel(self);
+  };
+  
+  // Turns on the panel 
+  self.turnOn = function () {
+    $scope.isCollapsed = false;
+  };
+  
+  // Turns off the panel 
+  self.turnOff = function () {
+    $scope.isCollapsed = true;
+  };
+  
+  $scope.toggle = function() {
+    // $scope.isCollapsed = !$scope.isCollapsed;
+    self.parent.selectPanel(self,$scope.isCollapsed);
+  };
+})
+.component('accordionPanel', {
+  require: {
+    'parent': '^accordion'
+  },
+  bindings: {
+    heading: '@'
+  },
+  transclude: true,
+  templateUrl: 'js/components/accordion/accordionPanelTmpl.html',
+  controller: 'AccordionPanelCtrl'
+})
+.controller('MyCtrl', function($scope, $element, $attrs) {
+  var element = $element[0];
+  
+  $scope.$watch($attrs.collapse, function (collapse) {
+    
+    var newHeight = collapse ? 0 : getElementAutoHeight();
+
+		element.style.height = newHeight + 'px';
+  })
+  
+  function getElementAutoHeight() {
+    var currentHeight = getElementCurrentHeight();
+    
+    element.style.height = 'auto';
+    var autoHeight = getElementCurrentHeight();
+    
+    element.style.height = currentHeight;
+    // Force the browser to recalc height after moving it back to normal
+    getElementCurrentHeight(); 
+    
+    return autoHeight;
+  }
+  
+  function getElementCurrentHeight() {
+    return element.offsetHeight
+  }
+})
+
 angular
 .module('app')
 .component('taskSingleChoiceImg', {
